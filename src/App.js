4170afab20193355e0e7dfca0af4bdd9643651dc
Paras <sparas1999@gmail.com>
2022-05-28 14:23:58 +0530

    final update

--- src/App.js
+++ src/App.js
@@ -4,19 +4,12 @@ import { Routes, Route } from "react-router-dom";
 import SignUp from "./Component/SignUp";
 import ForgetPassword from "./Component/ForgetPassword";
 import { AuthProvider } from "./AuthContext";
-// import { auth } from "./firebase";
-// import { useAuth } from "./AuthContext";
-// import Navbar from "./Component/Navbar/Navbar";
 import AboutUs from "./Component/AboutUs/AboutUs";
 import HomePage from "./Component/HomePage";
 import Profile from "./Component/Profile/Profile";
 import EditProfile from "./Component/Profile/EditProfile";
 import LandingPage from "./Component/LandPage/LandingPage";
 import CreateSession from "./Component/CreateSession/CreateSession";
-// import PrivateRouteAboutus from "./Component/PrivateRoutes/PrivateRouteAboutus";
-// import PrivateRouteHomepage from "./Component/PrivateRoutes/PrivateRouteHomepage";
-// import PrivateRouteProfile from "./Component/PrivateRoutes/PrivateRouteProfile";
-// import PrivateRouteEditProfile from "./Component/PrivateRoutes/PrivateRouteEditProfile";
 import Article from "./Component/Article/Article";
 import ConfirmPage from "./Component/ConfirmPage";
 import Blogs from "./Component/Blogs/Blogs";
@@ -68,25 +61,19 @@ const App = () => {
           <Route
             path="/Aboutus"
             element={
-              // <PrivateRouteAboutus>
               <AboutUs />
-              // </PrivateRouteAboutus>
             }
             />
           <Route
             path="/Profile"
             element={
-              // <PrivateRouteProfile>
               <Profile />
-              // </PrivateRouteProfile>
             }
           />
           <Route
             path="/EditProfile"
             element={
-              // <PrivateRouteEditProfile>
               <EditProfile />
-              // </PrivateRouteEditProfile>
             }
             />
           <Route path="/Homepage/article" element={<Article />} />
@@ -94,9 +81,7 @@ const App = () => {
           <Route
             path="/blogs"
             element={
-              // <PrivateRouteEditProfile>
               <Blogs />
-              // </PrivateRouteEditProfile>
             }
             />
 
